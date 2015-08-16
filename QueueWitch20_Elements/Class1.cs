fa1c8b6adaa36d1155e56b22806b72e33c0d50aa
Marek Bialozyt <marek.bialozyt@outlook.com>
2015-08-16 17:35:40 +0200

    Class ver 1.0
    Class in C # supports a queue of limited size (20 elements). Each
    element added to the queue should have a certain value (an integer in
    the range 1-10). From the queue can be obtained element with the highest
    value. In the case of overflow queue (adding 21 element) will be removed
    from the queue element with the smallest value. Reading the item deletes
    the downloaded item from the queue.

---
+++ QueueWitch20_Elements/Class1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueWitch20_Elements
+{
+    /// <summary>
+    /// Sorted queue with 20 elements. 
+    /// The addition of more than twenty elements will erase the element with the lowest value.
+    /// Elements must be from range 1-10
+    /// </summary>
+    class Queue20_Elements
+    {
+
+        private const int _maxLenght = 19;
+        private int _actualPosition;
+        private int[] _tablica;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Array20_Elements"/> class.
+        /// </summary>
+        public Queue20_Elements()
+        {
+            _actualPosition = -1;
+            _tablica = new int[_maxLenght + 1];
+        }
+
+        /// <summary>
+        /// Adds the specified value.
+        /// </summary>
+        /// <param name="value">The value. An integer from 1 to 10</param>
+        /// <exception cref="ArgumentException">Too big or too low value. Value must be from the range 1-10.</exception>
+        public void Add(int value)
+        {
+            if (value > 0 && value < 11)
+            {
+                if (_actualPosition + 1 <= _maxLenght)
+                {
+                    //add position
+                    _actualPosition++;
+                    _tablica[_actualPosition] = value;
+                    if (_actualPosition > 0)
+                    {
+                        sortuj();
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < _actualPosition; i++)
+                    {
+                        _tablica[i] = _tablica[i + 1];
+                    }
+                    _tablica[_actualPosition] = value;
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Too big or too low value. Value must be from the range 1-10.");
+            }
+        }
+
+
+        /// <summary>
+        /// Gets this instance.
+        /// </summary>
+        /// <returns>Returns the currently highest stored value and deletes it from the queue.</returns>
+        /// <exception cref="IndexOutOfRangeException">When you try to get value and the queue is empty.</exception>
+        public int Get()
+        {
+            if (_actualPosition >= 0)
+            {
+                int odczyt = _tablica[_actualPosition];
+                _actualPosition--;
+                return odczyt;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+        }
+
+        /// <summary>
+        /// Lenghts this instance.
+        /// </summary>
+        /// <returns>Returns number of positions in queue.</returns>
+        public int Lenght()
+        {
+            return _actualPosition;
+        }
+
+        /// <summary>
+        /// Sorts this instance.
+        /// </summary>
+        private void sortuj()
+        {
+            int n = _actualPosition + 1;
+            do
+            {
+                for (int i = 0; i < n - 1; i++)
+                {
+                    if (_tablica[i] > _tablica[i + 1])
+                    {
+                        int tmp = _tablica[i];
+                        _tablica[i] = _tablica[i + 1];
+                        _tablica[i + 1] = tmp;
+                    }
+                }
+                n--;
+            }
+            while (n > 1);
+        }
+
+
+    }
+}
